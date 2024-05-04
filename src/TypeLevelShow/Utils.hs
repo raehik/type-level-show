fac219dbe3b96bed78b1bf5948e00e1603e88c43
Ben Orchard <thefirstmuffinman@gmail.com>
2024-05-05 00:20:38 +0100

    add more utils

--- src/TypeLevelShow/Utils.hs
+++ src/TypeLevelShow/Utils.hs
@@ -9,6 +9,21 @@ import GHC.TypeLits
 type ShowParen :: Bool -> Symbol -> Symbol
 type family ShowParen b p where
     ShowParen False p = p
-    ShowParen True  p = ConsSymbol '(' p ++ ")"
+    ShowParen True  p = ConsSymbol '(' p `AppendSymbol` ")"
+
+-- | Surround the given 'Symbol' with parentheses if the given precedence is
+--   greater than the given 'Natural'.
+--
+-- Type-level relational operations are a pain. This may be easier to use.
+type ShowParenIfGt :: Natural -> Natural -> Symbol -> Symbol
+type ShowParenIfGt n d p = ShowParen (OrderingLT (CmpNat d n)) p
+
+type family OrderingLT (a :: Ordering) :: Bool where
+    OrderingLT LT = True
+    OrderingLT EQ = False
+    OrderingLT GT = False
+
+type ShowChar ch = ConsSymbol ch ""
 
 type l ++ r = AppendSymbol l r
+type l >  r = OrderingLT (CmpNat l r)
