39125e86138420b8cab12b6f4c0a7a6edb65deea
Ben Orchard <thefirstmuffinman@gmail.com>
2024-05-14 18:53:04 +0100

    Doc: +Render; refactor

--- src/TypeLevelShow/Doc.hs
+++ src/TypeLevelShow/Doc.hs
@@ -2,24 +2,40 @@
 
 module TypeLevelShow.Doc where
 
+import GHC.TypeLits qualified as TE -- TE = TypeError
 import GHC.TypeLits ( Symbol, KnownSymbol, symbolVal' )
 import GHC.Exts ( proxy# )
 
 -- | Simple pretty document ADT.
 --
--- Designed to work on both type level (as a simplified
--- 'GHC.TypeLits.ErrorMessage') and term level (as a boring ADT).
+-- Designed to work on both type level (as a limited 'TE.ErrorMessage') and term
+-- level (as a boring ADT).
 --
--- Note that 'GHC.TypeLits.ErrorMessage.ShowType' is magical (see
+-- Note that 'TE.ShowType' is magical (see
 -- @compiler/GHC/Core/Type.hs#L1309@), so we need to remove it for term level.
+--
+-- singletons-base defines a version of this, but retains the 'TE.ShowType'
+-- constructor and is in the singletons ecosystem.
 data Doc s
-  = Text' s
+  = Text s
   -- ^ plain ol' text
-  | Doc s :<>.: Doc s
+  | Doc s :<>: Doc s
   -- ^ append docs next to each other
-  | Doc s :$$.: Doc s
+  | Doc s :$$: Doc s
   -- ^ stack docs on top of each other (newline)
     deriving stock Show
+
+-- | Promoted 'Doc'.
+type PDoc = Doc Symbol
+
+-- | Render a 'PDoc' as an 'ErrorMessage', for type-level error messages.
+--
+-- 'PDoc' is a subset of 'ErrorMessage', so this is very boring.
+type RenderDoc :: PDoc -> TE.ErrorMessage
+type family RenderDoc doc where
+    RenderDoc (Text s)   = TE.Text s
+    RenderDoc (l :<>: r) = RenderDoc l TE.:<>: RenderDoc r
+    RenderDoc (l :$$: r) = RenderDoc l TE.:$$: RenderDoc r
 
 -- | Reify a type-level 'Doc' to the corresponding term-level one.
 class ReifyDoc (doc :: Doc Symbol) where
@@ -27,14 +43,14 @@ class ReifyDoc (doc :: Doc Symbol) where
     --      performance, rather than mapping afterwards? hard to say
     reifyDoc :: Doc String
 
-instance KnownSymbol s => ReifyDoc (Text' s) where
-    reifyDoc = Text' $ symbolVal' (proxy# @s)
+instance KnownSymbol s => ReifyDoc (Text s) where
+    reifyDoc = Text $ symbolVal' (proxy# @s)
 
-instance (ReifyDoc l, ReifyDoc r) => ReifyDoc (l :<>.: r) where
-    reifyDoc = reifyDoc @l :<>.: reifyDoc @r
+instance (ReifyDoc l, ReifyDoc r) => ReifyDoc (l :<>: r) where
+    reifyDoc = reifyDoc @l :<>: reifyDoc @r
 
-instance (ReifyDoc l, ReifyDoc r) => ReifyDoc (l :$$.: r) where
-    reifyDoc = reifyDoc @l :$$.: reifyDoc @r
+instance (ReifyDoc l, ReifyDoc r) => ReifyDoc (l :$$: r) where
+    reifyDoc = reifyDoc @l :$$: reifyDoc @r
 
 {-
 
