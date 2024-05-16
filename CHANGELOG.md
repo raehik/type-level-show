8d7986d7c5dfb677cea045ea7cedb4edc2b9e334
Ben Orchard <thefirstmuffinman@gmail.com>
2024-05-16 04:50:37 +0100

    release 0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 0.1.1 (2024-05-16)
+* add `Doc` type for `ErrorMessage`-like errors, except term level also
+
 ## 0.1.0 (2024-05-07)
 Initial release.
 
--- src/TypeLevelShow/Doc.hs
+++ src/TypeLevelShow/Doc.hs
@@ -37,8 +37,8 @@ type family RenderDoc doc where
     RenderDoc (l :<>: r) = RenderDoc l TE.:<>: RenderDoc r
     RenderDoc (l :$$: r) = RenderDoc l TE.:$$: RenderDoc r
 
--- | Reify a type-level 'Doc' to the corresponding term-level one.
-class ReifyDoc (doc :: Doc Symbol) where
+-- | Reify a promoted 'Doc' to the corresponding term-level one.
+class ReifyDoc (doc :: PDoc) where
     -- TODO do we want to do IsString directly in here? will it guarantee better
     --      performance, rather than mapping afterwards? hard to say
     reifyDoc :: Doc String
@@ -51,46 +51,3 @@ instance (ReifyDoc l, ReifyDoc r) => ReifyDoc (l :<>: r) where
 
 instance (ReifyDoc l, ReifyDoc r) => ReifyDoc (l :$$: r) where
     reifyDoc = reifyDoc @l :$$: reifyDoc @r
-
-{-
-
--- | 'Doc' singleton type.
-data SDoc (doc :: Doc Symbol) where
-    SText'   :: SSymbol s -> SDoc (Text' s)
-    (:%<.>:) :: SDoc l -> SDoc r -> SDoc (l :<.>: r)
-    (:%$.$:) :: SDoc l -> SDoc r -> SDoc (l :$.$: r)
-
-data SomeSDoc where SomeSDoc :: SDoc doc -> SomeSDoc
-
-fromSDoc :: SDoc doc -> Doc String
-fromSDoc = \case
-  SText' s   -> Text' $ fromSSymbol s
-  l :%<.>: r -> fromSDoc l :<.>: fromSDoc r
-  l :%$.$: r -> fromSDoc l :$.$: fromSDoc r
-
-toSDoc :: Doc String -> SomeSDoc
-toSDoc = \case
-  Text' s   -> withSomeSSymbol s $ SomeSDoc . SText'
-  l :<.>: r -> withSomeSDoc l $ \sl -> withSomeSDoc r $ \sr ->
-    SomeSDoc $ sl :%<.>: sr
-  l :$.$: r -> withSomeSDoc l $ \sl -> withSomeSDoc r $ \sr ->
-    SomeSDoc $ sl :%$.$: sr
-
-withSomeSDoc :: Doc String -> (forall doc. SDoc doc -> r) -> r
-withSomeSDoc doc f = case toSDoc doc of SomeSDoc doc' -> f doc'
-
---reifyDoc' :: forall (doc :: Doc Symbol). Doc String
---reifyDoc' = fromSDoc (toSDoc 
-
-{-
-data SomeDoc where
-    --SomeDoc :: Proxy# (doc :: Doc Symbol) -> SomeDoc
-    SomeDoc :: ReifyDoc doc => SomeDoc
-
-toSingDoc :: Doc String -> SomeDoc
-toSingDoc = \case
-  Text' s -> withSomeSSymbol s $ \(_ :: SSymbol s) ->
-    SomeDoc @(Text' s)
--}
-
--}
