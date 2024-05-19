734c5ac372a58ccf4b15021725fde1c92fb04185
Ben Orchard <thefirstmuffinman@gmail.com>
2024-05-19 15:50:28 +0100

    Doc: +SDoc

--- src/TypeLevelShow/Doc.hs
+++ src/TypeLevelShow/Doc.hs
@@ -3,8 +3,7 @@
 module TypeLevelShow.Doc where
 
 import GHC.TypeLits qualified as TE -- TE = TypeError
-import GHC.TypeLits ( Symbol, KnownSymbol, symbolVal' )
-import GHC.Exts ( proxy# )
+import GHC.TypeLits hiding ( ErrorMessage(..) )
 
 -- | Simple pretty document ADT.
 --
@@ -31,23 +30,38 @@ type PDoc = Doc Symbol
 -- | Render a 'PDoc' as an 'ErrorMessage', for type-level error messages.
 --
 -- 'PDoc' is a subset of 'ErrorMessage', so this is very boring.
-type RenderDoc :: PDoc -> TE.ErrorMessage
-type family RenderDoc doc where
-    RenderDoc (Text s)   = TE.Text s
-    RenderDoc (l :<>: r) = RenderDoc l TE.:<>: RenderDoc r
-    RenderDoc (l :$$: r) = RenderDoc l TE.:$$: RenderDoc r
+type RenderPDoc :: PDoc -> TE.ErrorMessage
+type family RenderPDoc doc where
+    RenderPDoc (Text s)   = TE.Text s
+    RenderPDoc (l :<>: r) = RenderPDoc l TE.:<>: RenderPDoc r
+    RenderPDoc (l :$$: r) = RenderPDoc l TE.:$$: RenderPDoc r
+
+-- | Singleton 'Doc'.
+data SDoc (doc :: PDoc) where
+    SText   :: SSymbol s -> SDoc (Text s)
+    (:$<>:) :: SDoc docl -> SDoc docr -> SDoc (docl :<>: docr)
+    (:$$$:) :: SDoc docl -> SDoc docr -> SDoc (docl :$$: docr)
+
+-- | Produce the singleton for the given promoted 'Doc'.
+class SingDoc (doc :: PDoc) where
+    singDoc :: SDoc doc
+
+instance KnownSymbol s => SingDoc (Text s) where
+    singDoc = SText (SSymbol @s)
+
+instance (SingDoc l, SingDoc r) => SingDoc (l :<>: r) where
+    singDoc = singDoc @l :$<>: singDoc @r
+
+instance (SingDoc l, SingDoc r) => SingDoc (l :$$: r) where
+    singDoc = singDoc @l :$$$: singDoc @r
+
+-- | Demote an 'SDoc' singleton to the corresponding base 'Doc'.
+demoteDoc :: SDoc doc -> Doc String
+demoteDoc = \case
+  SText s   -> Text $ fromSSymbol s
+  l :$<>: r -> demoteDoc l :<>: demoteDoc r
+  l :$$$: r -> demoteDoc l :$$: demoteDoc r
 
 -- | Reify a promoted 'Doc' to the corresponding term-level one.
-class ReifyDoc (doc :: PDoc) where
-    -- TODO do we want to do IsString directly in here? will it guarantee better
-    --      performance, rather than mapping afterwards? hard to say
-    reifyDoc :: Doc String
-
-instance KnownSymbol s => ReifyDoc (Text s) where
-    reifyDoc = Text $ symbolVal' (proxy# @s)
-
-instance (ReifyDoc l, ReifyDoc r) => ReifyDoc (l :<>: r) where
-    reifyDoc = reifyDoc @l :<>: reifyDoc @r
-
-instance (ReifyDoc l, ReifyDoc r) => ReifyDoc (l :$$: r) where
-    reifyDoc = reifyDoc @l :$$: reifyDoc @r
+reifyDoc :: forall (doc :: PDoc). SingDoc doc => Doc String
+reifyDoc = demoteDoc (singDoc @doc)
