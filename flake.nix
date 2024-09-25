c20b27cb27ee5541caa98b37e35838ba7b28e753
Ben Orchard <thefirstmuffinman@gmail.com>
2024-09-25 04:34:00 +0100

    update Nix flake
    GHC 9.10 support is just waiting on a defun-core update.

--- flake.nix
+++ flake.nix
@@ -1,22 +1,13 @@
-# TODO
-# * better devshell name overriding. clumsy because we can't access the
-#   derivation being used (because it's auto-grabbed). really just wanna change
-#   `ghc-shell-for` to `ghcXY` and keep the `-${pname}-${version}`!
-# * honestly maybe I move away from haskell-flake...? it's weird
-
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     haskell-flake.url = "github:srid/haskell-flake";
-    singleraeh.url   = "github:raehik/singleraeh";
-    singleraeh.flake = false;
   };
   outputs = inputs:
   let
-    # simple devshell for non-dev compilers: really just want `cabal repl`
-    nondevDevShell = compiler: {
-      mkShellArgs.name = "${compiler}-type-level-show";
+    defDevShell = compiler: {
+      mkShellArgs.name = "${compiler}";
       hoogle = false;
       tools = _: {
         hlint = null;
@@ -29,27 +20,21 @@
       systems = inputs.nixpkgs.lib.systems.flakeExposed;
       imports = [ inputs.haskell-flake.flakeModule ];
       perSystem = { self', pkgs, config, ... }: {
-        packages.default  = self'.packages.ghc96-type-level-show;
-        devShells.default = self'.devShells.ghc96;
+        packages.default  = self'.packages.ghc98-type-level-show;
+        devShells.default = self'.devShells.ghc98;
+        haskellProjects.ghc910 = {
+          basePackages = pkgs.haskell.packages.ghc910;
+          # v https://github.com/phadej/defun/pull/5
+          settings.defun-core.jailbreak = true;
+          devShell = defDevShell "ghc910";
+        };
         haskellProjects.ghc98 = {
           basePackages = pkgs.haskell.packages.ghc98;
-          packages.singleraeh.source = inputs.singleraeh;
-          devShell = nondevDevShell "ghc98";
+          devShell = defDevShell "ghc98";
         };
         haskellProjects.ghc96 = {
           basePackages = pkgs.haskell.packages.ghc96;
-          packages.singleraeh.source = inputs.singleraeh;
-          devShell.mkShellArgs.name = "ghc96-type-level-show";
-        };
-        haskellProjects.ghc94 = {
-          basePackages = pkgs.haskell.packages.ghc94;
-          packages.singleraeh.source = inputs.singleraeh;
-          devShell = nondevDevShell "ghc94";
-        };
-        haskellProjects.ghc92 = {
-          basePackages = pkgs.haskell.packages.ghc92;
-          packages.singleraeh.source = inputs.singleraeh;
-          devShell = nondevDevShell "ghc92";
+          devShell = defDevShell "ghc96";
         };
       };
     };
